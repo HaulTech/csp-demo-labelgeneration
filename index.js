a7369e620929bf578868b4ac1e528dca3479dfc4
Patch <patrick.fegan@haultech.co.uk>
2022-06-14 16:04:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const axios = require('axios');
 const fs = require('fs');
 
 /* Per CSP Variables */
-let apiUrl = '';
-let token = '';
-let tmsJobId = -1;
+let apiUrl = ''; // Root API url, e.g. https://api-haultechdemo.httms.uk
+let token = ''; // Can be retreived using https://github.com/HaulTech/csp-demo-oauthtoken
+let jobId = 1; // The confirmed Job ID as returned from /api/Job/Confirm
 
 /* Below is a simple example of retreiving a PDF lebel */
-axios.get(`${apiUrl}/api/Label/GenerateByTmsId/${tmsJobId}`, {
+axios.get(`${apiUrl}/api/Label/GenerateByTmsId/${jobId}`, {
   headers: { 'Authorization': `Bearer ${token}` }
 })
   .then((response) => {
@@ -15,7 +15,7 @@ axios.get(`${apiUrl}/api/Label/GenerateByTmsId/${tmsJobId}`, {
     console.log('PDF generated successfully.');
     axios.get(labelUrl).then(response => {
       try {
-        fs.writeFileSync(`${tmsJobId}.pdf`, response.data);
+        fs.writeFileSync(`${jobId}.pdf`, response.data);
         console.log('PDF downloaded successfully.');
       } catch (error) {
         console.error('Failed to write PDF.');
